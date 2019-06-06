c8482dab405b15c6f465d49a2291f3b7c87404fc
acr <acarbey15@gmail.com>
2019-06-06 13:39:56 +0300

    re

---
+++ build/precache-manifest.3bc0a5d7461d3a215c0693238b7a8456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ce4f71cec191632dde0",
+    "url": "/static/js/main.b234f891.chunk.js"
+  },
+  {
+    "revision": "4b295e258f033d84802c",
+    "url": "/static/js/2.0b843299.chunk.js"
+  },
+  {
+    "revision": "2ce4f71cec191632dde0",
+    "url": "/static/css/main.579b5558.chunk.css"
+  },
+  {
+    "revision": "f0f61a609ad8f7b15bc0ad7ba2acc242",
+    "url": "/index.html"
+  }
+];
