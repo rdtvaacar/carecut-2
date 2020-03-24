eb10cc0f233234f111dd9ec85969360c0141eb0b
acr <acarbey15@gmail.com>
2020-03-25 00:21:50 +0300

    re

---
+++ precache-manifest.4fb5e7c4360817402e048a0d87b1a250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+    {
+        "revision": "42ac5946195a7306e2a5",
+        "url": "/static/js/runtime~main.js"
+    },
+    {
+        "revision": "38f46e5ee6bbd6965d8c",
+        "url": "/static/js/main_chunk.js"
+    },
+    {
+        "revision": "ec9172a088d8b5a79bbc",
+        "url": "/static/js/chunk.js"
+    },
+    {
+        "revision": "38f46e5ee6bbd6965d8c",
+        "url": "/static/css/main_chunk.css"
+    },
+    {
+        "revision": "39893125507844e87ecb9276e1a6a248",
+        "url": "/index.html"
+    }
+];
